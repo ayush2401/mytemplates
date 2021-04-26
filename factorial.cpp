1532a8c4393e6ca7ffe2c89de14561936d1746bb
ayush saraf <55061546+ayush2401@users.noreply.github.com>
2021-04-26 15:24:09 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+ll add(ll a, ll b)  { return ((a % mod + b % mod) % mod); }
+ll sub(ll a, ll b)  { return ((a % mod - b % mod) % mod); }
+ll mult(ll a, ll b) { return (((a % mod) * (b % mod)) % mod); }
+
+
+const ll mxn = 3e5 + 5;
+ll fact[mxn] , rev[mxn];
+
+ll power(ll a, ll b, ll m, ll ans = 1) {
+    for (; b ; b >>= 1, a = 1LL * a * a % m) if (b & 1) ans = 1LL * ans * a % m;
+    return ans;
+}
+
+void factorial()
+{
+    fact[0] = rev[0] = 1;
+    for (ll i = 1; i < mxn; i++) fact[i] = fact[i - 1] * i % mod;
+    rev[mxn - 1] = power(fact[mxn - 1], mod - 2, mod);
+    for (ll i = mxn - 2; i >= 1; i--) rev[i] = rev[i + 1] * (i + 1) % mod;
+}
